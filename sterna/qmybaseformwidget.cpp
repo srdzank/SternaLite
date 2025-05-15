16d86e3b48975ec9333596fcd2bd9b01869f9f86
srdzank <srdzan.kostenarov@gmail.com>
2025-05-15 10:10:33 +0200

    Zoom in out

--- sterna/qmybaseformwidget.cpp
+++ sterna/qmybaseformwidget.cpp
@@ -1,59 +1,118 @@
 #include "qmybaseformwidget.h"
 #include <QKeyEvent>
+#include <QPainter>
+#include <QTableView>
+#include <QWheelEvent>
+#include <QApplication>
 
 QMyBaseFormWidget::QMyBaseFormWidget(QWidget *parent)
-	: QWidget(parent)
-    ,m_Widget(0)
+    : QWidget(parent), m_Widget(nullptr), m_zoomFactor(1.0)
 {
-	ui.setupUi(this);
-	setMouseTracking(true);
+    ui.setupUi(this);
+    setMouseTracking(true);
 }
 
 QMyBaseFormWidget::~QMyBaseFormWidget()
 {
-
 }
 
 void QMyBaseFormWidget::keyPressEvent(QKeyEvent *event)
 {
-	switch (event->key()) 
-	{
-	case Qt::Key_Escape: 
-		pressEscape(); break;
-	case Qt::Key_Return: 
-		pressReturn(); break;
-	case Qt::Key_Enter: 
-		pressReturn(); break;
-	case Qt::Key_F9: 
-		pressF9(); break;
+    if (event->modifiers() & Qt::ControlModifier)
+    {
+        switch (event->key()) {
+        case Qt::Key_Plus:
+        case Qt::Key_Equal:
+            zoomIn(); return;
+        case Qt::Key_Minus:
+            zoomOut(); return;
+        case Qt::Key_0:
+            resetZoom(); return;
+        }
+    }
 
-	default:
-		QWidget::keyPressEvent(event);
-	}
+    switch (event->key())
+    {
+    case Qt::Key_Escape:
+        pressEscape(); break;
+    case Qt::Key_Return:
+    case Qt::Key_Enter:
+        pressReturn(); break;
+    case Qt::Key_F9:
+        pressF9(); break;
+    default:
+        QWidget::keyPressEvent(event);
+    }
 }
 
-void QMyBaseFormWidget::mouseDoubleClickEvent(QMouseEvent * event)
+void QMyBaseFormWidget::mouseDoubleClickEvent(QMouseEvent *event)
 {
-	pressReturn();
+    pressReturn();
 }
 
-void QMyBaseFormWidget::pressEscape()
+void QMyBaseFormWidget::wheelEvent(QWheelEvent *event)
 {
-}
-void QMyBaseFormWidget::refresh()
-{
+    if (QApplication::keyboardModifiers() == Qt::ControlModifier)
+    {
+        if (event->angleDelta().y() > 0)
+            zoomIn();
+        else
+            zoomOut();
+        event->accept();
+        return;
+    }
+
+    QWidget::wheelEvent(event);
 }
 
-void QMyBaseFormWidget::pressReturn()
+void QMyBaseFormWidget::pressEscape() {}
+void QMyBaseFormWidget::refresh() {}
+void QMyBaseFormWidget::pressReturn() {}
+void QMyBaseFormWidget::pressF9() {}
+
+void QMyBaseFormWidget::zoomIn()
 {
+    scaleWidgets(1.1);
+    m_zoomFactor *= 1.1;
 }
 
-void QMyBaseFormWidget::pressF9()
+void QMyBaseFormWidget::zoomOut()
 {
+    scaleWidgets(0.9);
+    m_zoomFactor *= 0.9;
 }
 
-
-void QMyBaseFormWidget::paintEvent ( QPaintEvent * event )
+void QMyBaseFormWidget::resetZoom()
 {
-	// paint here
+    scaleWidgets(1.0 / m_zoomFactor);
+    m_zoomFactor = 1.0;
+}
+
+void QMyBaseFormWidget::scaleWidgets(double factor)
+{
+    QList<QWidget *> widgets = this->findChildren<QWidget *>();
+
+    for (QWidget *w : widgets)
+    {
+        QFont f = w->font();
+        f.setPointSizeF(f.pointSizeF() * factor);
+        w->setFont(f);
+
+        QSize sz = w->size();
+        if (!sz.isEmpty())
+        {
+            w->resize(sz * factor);
+        }
+
+        if (auto tv = qobject_cast<QTableView *>(w))
+        {
+            for (int col = 0; col < tv->model()->columnCount(); ++col)
+            {
+                int cw = tv->columnWidth(col);
+                tv->setColumnWidth(col, int(cw * factor));
+            }
+        }
+    }
+
+    this->resize(this->size() * factor);
 }
--- sterna/qmybaseformwidget.h
+++ sterna/qmybaseformwidget.h
@@ -5,69 +5,80 @@
 #include "ui_qmybaseformwidget.h"
 #include <QDateTime>
 #include <QPainter>
+#include <QKeyEvent>
+#include <QWheelEvent>
 
 class QMyBaseFormWidget : public QWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	typedef struct {
-		QString faktura_id;
-		QString faktura_datum;
-		QString faktura_komintent_id;
-		QString faktura_komintent;
-		QString faktura_iznos;
-		QString faktura_ddv;
-		QString faktura_vkupenIznos;
-		QString faktura_valuta;
-		QString faktura_iznos_1;
-		QString faktura_iznos_2;
-		QString faktura_iznos_3;
-		QString faktura_iznos_4;
-		QString faktura_iznos_5;
-		QString faktura_iznos_6;
-	} izlFakturaData;
+    typedef struct {
+        QString faktura_id;
+        QString faktura_datum;
+        QString faktura_komintent_id;
+        QString faktura_komintent;
+        QString faktura_iznos;
+        QString faktura_ddv;
+        QString faktura_vkupenIznos;
+        QString faktura_valuta;
+        QString faktura_iznos_1;
+        QString faktura_iznos_2;
+        QString faktura_iznos_3;
+        QString faktura_iznos_4;
+        QString faktura_iznos_5;
+        QString faktura_iznos_6;
+    } izlFakturaData;
 
-	typedef struct 
-	{
-		QDateTime fakturaData;
-		QDateTime fakturaValuta;
-		float iznos;
-		float plateno;
-		QString komintent_naziv;
-		QString komintent_id;
-	} tinfoFakturi;
-	
-	typedef struct 
-	{
-		QString komintent_id;
-		QString komintent_naziv;
-		QString faktura_tip;
-		QString faktura_id;
-		QString datum;
-		QString neplaten_iznos;
-		QString plateno;
-	} sostavInfo;
-	
-	typedef std::vector<tinfoFakturi> tListInfoFakturi;
-	typedef std::map<QString , std::vector<sostavInfo>> type_m_map;
-	QMyBaseFormWidget(QWidget *parent = 0);
-	~QMyBaseFormWidget();
-    void setCategoryWidget(QWidget * mWidget){m_Widget = mWidget;}
-    QWidget *getCategoryWidget(){return m_Widget;}
+    typedef struct
+    {
+        QDateTime fakturaData;
+        QDateTime fakturaValuta;
+        float iznos;
+        float plateno;
+        QString komintent_naziv;
+        QString komintent_id;
+    } tinfoFakturi;
+
+    typedef struct
+    {
+        QString komintent_id;
+        QString komintent_naziv;
+        QString faktura_tip;
+        QString faktura_id;
+        QString datum;
+        QString neplaten_iznos;
+        QString plateno;
+    } sostavInfo;
+
+    typedef std::vector<tinfoFakturi> tListInfoFakturi;
+    typedef std::map<QString , std::vector<sostavInfo>> type_m_map;
+
+    QMyBaseFormWidget(QWidget *parent = nullptr);
+    virtual ~QMyBaseFormWidget();
+
+    void setCategoryWidget(QWidget * mWidget) { m_Widget = mWidget; }
+    QWidget *getCategoryWidget() { return m_Widget; }
+
+public slots:
+    void zoomIn();
+    void zoomOut();
+    void resetZoom();
+
 protected:
-	virtual void pressEscape();
-	virtual void pressReturn();
-	virtual void mouseDoubleClickEvent(QMouseEvent * event);
-	virtual void refresh();
-	virtual void pressF9();
-	void paintEvent ( QPaintEvent * event );
+    virtual void pressEscape();
+    virtual void pressReturn();
+    virtual void refresh();
+    virtual void pressF9();
+    virtual void mouseDoubleClickEvent(QMouseEvent * event) override;
+    virtual void keyPressEvent(QKeyEvent *event) override;
+    virtual void wheelEvent(QWheelEvent *event) override;
 
 private:
-	Ui::QMyBaseFormWidgetClass ui;
-	void keyPressEvent(QKeyEvent *event);
-    QWidget * m_Widget;
-
+    Ui::QMyBaseFormWidgetClass ui;
+    QWidget *m_Widget;
+    double m_zoomFactor = 1.0;
+    void scaleWidgets(double factor);
 };
 
 #endif // QMYBASEFORMWIDGET_H
